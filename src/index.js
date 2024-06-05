5b55065846546a1c5eb81da31499be099593d3ba
nailsonxavierr <nx.nailsonxavier@gmail.com>
2024-06-05 12:04:22 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+// import models from "./models";
+import models, { sequelize } from "./models";
+import routes from "./routes";
+
+console.log("Projeto Node + Express - 2023.2");
+console.log("MY_SECRET", process.env.MY_SECRET);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(async (req, res, next) => {
+  req.context = {
+    models,
+    me: await models.User.findByLogin("rwieruch"),
+  };
+  next();
+});
+
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
+
+app.get("/", (req, res) => {
+  return res.send("Hello Express");
+});
+
+const port = process.env.PORT || 3000;
+const eraseDatabaseOnSync = process.env.ERASE_DATABASE_ON_SYNC === "true";
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(() => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+  app.listen(port, () => console.log(`Example app listening on port ${port}`));
+});
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: "rwieruch",
+      messages: [
+        {
+          text: "Published the Road to learn React",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+
+  await models.User.create(
+    {
+      username: "ddavids",
+      messages: [
+        {
+          text: "Happy to release ...",
+        },
+        {
+          text: "Published a complete ...",
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    }
+  );
+};
